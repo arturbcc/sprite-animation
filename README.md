381026aded9b7b6106f9e5d3e7b8ca77746cbbec
Artur <artur.prado@abril.com.br>
2015-09-11 17:09:20 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+http://blaiprat.github.io/jquery.animateSprite/
+http://www.spriters-resource.com/pc_computer/marvelavengers/
---
+++ javascripts/animate.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('.captain-america').animateSprite({
+    fps: 10,
+    columns: 4,
+    width: 210,
+    height: 200,
+    animations: {
+        walkRight: [0, 1, 2, 3, 2, 1],
+        walkLeft: [15, 14, 13, 12, 11, 10, 9, 8]
+    },
+    loop: true,
+    complete: function() {
+        // use complete only when you set animations with 'loop: false'
+        console.log('animation End');
+    }
+  });
+});
